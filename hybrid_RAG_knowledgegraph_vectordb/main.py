2654f51fb3a5d24230e1e72b0aaf26c9c4b2b911
Ali Shamsaddinlou <yadet.alishamsaddinlou@gmail.com>
2024-07-16 11:00:21 +0200

    data to be indexed found

--- hybrid_RAG_knowledgegraph_vectordb/main.py
+++ hybrid_RAG_knowledgegraph_vectordb/main.py
@@ -1,8 +1,10 @@
 from llama_index.llms.together import TogetherLLM
 from llama_index.embeddings.together import TogetherEmbedding
+from llama_index.core import SimpleDirectoryReader
 import logging
 import sys
 import os
+import requests
 import nest_asyncio
 from llama_index.core import Settings
 from IPython.display import Markdown, display
@@ -26,3 +28,23 @@ embed_model = TogetherEmbedding(
 
 Settings.embed_model = embed_model
 Settings.llm = llm
+
+
+# Create the directory
+os.makedirs('data', exist_ok=True)
+
+# Download the file
+url = 'https://www.gutenberg.org/cache/epub/2852/pg2852.txt'
+file_path = 'data/The Hound of the Baskervilles.txt'
+
+response = requests.get(url)
+with open(file_path, 'wb') as file:
+    file.write(response.content)
+
+# Load the document using SimpleDirectoryReader
+documents = SimpleDirectoryReader(
+    input_files=[file_path]
+).load_data()
+
+# Optional: Print the loaded documents to verify
+print(documents)
